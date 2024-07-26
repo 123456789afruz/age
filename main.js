15b8f5b32a69f591494e3b6dcf0bcc5502d90733
123456789afruz <afruzoxunov@gmail.com>
2024-07-26 11:22:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let a = prompt("How old are you?")
+
+if (a >= 100) {
+    console.log("ТЫ ЖИВОЙ?");
+}else if (a > 18) {
+    console.log("Ты совершеннолетний");
+}else if (a < 0) {
+    console.log("Ты еще не в планах");
+}else if (a == 0) {
+    console.log("Ты еще не родился");
+}else if (a < 18) {
+    console.log("Ты еще маленький");
+}
+ 
+    
